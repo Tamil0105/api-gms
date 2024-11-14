81cf2db7c22f5209fa4c5781f20471e0bbe84cc3
Tamil0105 <tamil16399@gmail.com>
2024-11-14 13:34:19 +0530

    main js updated

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+// Create a cached Express server instance
+const server = express();
+server.use(express.json()); // Parse JSON bodies
+let isAppInitialized = false;
 
-  app.enableCors(); // Enable CORS
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 8080);
+// Initialize the NestJS app within an Express server instance
+async function createApp() {
+  if (!isAppInitialized) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+    isAppInitialized = true;
+    console.log('NestJS application initialized');
+  } else {
+    console.log('NestJS application is already running');
+  }
+  return server;
 }
 
-bootstrap();
-console.log('app running in 8080')
+// Vercel serverless function entry point
+export default async (req: express.Request, res: express.Response) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  console.log('Request body:', req.body);
+
+  const app = await createApp();
+  app(req, res);
+};
