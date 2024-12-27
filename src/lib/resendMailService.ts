af8f4fac29652a908e5b9d19b76742a14ee9e500
Tamil0105 <tamil16399@gmail.com>
2024-12-27 20:44:38 +0530

    mail api changed

--- src/lib/resendMailService.ts
+++ src/lib/resendMailService.ts
@@ -6,16 +6,16 @@ import { Resend } from 'resend';
 export class MailService {
   private resend: any;
   constructor() {
-    this.resend = new Resend('re_CqJemkqa_DYTuCGLd44KoiJhDd5YkQrCL')
+    this.resend = new Resend('re_DGPuGWDS_QK9YmLVXsLe1LXbwfT2y8Kmz')
   }
   
 
-  async sendEmail(to: string, subject: string, body: string): Promise<any> {
+  async sendEmail(to: string,  subject: string, body: string): Promise<any> {
 
     try {
       //  const resend = new Resend('re_CqJemkqa_DYTuCGLd44KoiJhDd5YkQrCL'); // Initialize Resend instance
       const mailOptions = {
-        from: "contact@gameonsolution.in", // Sender's email address
+        from: "sales@gameonsolution.in", // Sender's email address
         to: to, // Recipient's email address
         subject: subject, // Subject line
         html: body, // HTML content
